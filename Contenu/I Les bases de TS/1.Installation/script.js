3328289e01491940558dbc7311fc1d3ef18559d8
unknown <lefortyop@gmail.com>
2022-07-26 22:09:16 +0200

    add npm .ts

---
+++ Contenu/I Les bases de TS/1.Installation/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let str = "Hello";
+str = "d2";
+let chiffre = 69;
+console.log("hello toi");
---
+++ Contenu/I Les bases de TS/1.Installation/script.ts
@@ -0,0 +1,7 @@
+let str = "Hello"
+
+str = "d2"
+
+let chiffre = 69;
+
+console.log("hello toi");
